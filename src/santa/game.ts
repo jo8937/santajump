0c8610ad053243cb7fc94aaf1abcf1967db394d1
Jihoon Cho <george@gamevilcom2us.com>
2018-08-27 17:54:44 +0900

    setting with boilerplate

---
+++ src/santa/game.ts
@@ -0,0 +1,32 @@
+/// <reference path="../phaser.d.ts"/>
+
+import "phaser";
+import { MainScene } from "./scenes/mainScene";
+
+// main game configuration
+const config: GameConfig = {
+  width: 600,
+  height: 800,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: MainScene,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  }
+};
+
+// game class
+export class Game extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
+
+// when the page is loaded, create our game instance
+window.onload = () => {
+  var game = new Game(config);
+};
+
---
+++ src/santa/scenes/mainScene.ts
@@ -0,0 +1,23 @@
+/**
+ * @author       Digitsensitive <digit.sensitivee@gmail.com>
+ * @copyright    2018 Digitsensitive
+ * @license      Digitsensitive
+ */
+
+export class MainScene extends Phaser.Scene {
+  private phaserSprite: Phaser.GameObjects.Sprite;
+
+  constructor() {
+    super({
+      key: "MainScene"
+    });
+  }
+
+  preload(): void {
+    this.load.image("logo", "./assets/180415-santa-sd-test.png");
+  }
+
+  create(): void {
+    this.phaserSprite = this.add.sprite(400, 300, "logo");
+  }
+}
