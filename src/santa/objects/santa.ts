f82f3a8a8f7bc70f443838aa2be909c4db0f6dd3
Jihoon Cho <george@gamevilcom2us.com>
2018-12-11 17:44:10 +0900

    make panel collider

--- src/santa/objects/santa.ts
+++ src/santa/objects/santa.ts
@@ -1,57 +1,66 @@
 declare var gyro: any;
 
 export class Santa extends Phaser.GameObjects.Sprite {
-    private jumpKey: Phaser.Input.Keyboard.Key;
-    private anim: Phaser.Tweens.Tween[];
-    private isDead: boolean = false;
-    private cursors : Phaser.Input.Keyboard.CursorKeys;
-    private pointers : Phaser.Input.Pointer;
-    private isPointerDown : boolean = false;
-    private vec : Vector2Like = {x:0,y:500};
-    private g : number = 0;
-    
-    public getDead(): boolean {
-      return this.isDead;
-    }
-  
-    public setDead(dead): void {
-      this.isDead = dead;
-    }
-  
-    constructor(params) {
-      super(params.scene, params.x, params.y, params.key, params.frame);
+  private jumpKey: Phaser.Input.Keyboard.Key;
+  private anim: Phaser.Tweens.Tween[];
+  private isDead: boolean = false;
+  private cursors: Phaser.Input.Keyboard.CursorKeys;
+  private pointers: Phaser.Input.Pointer;
+  private isPointerDown: boolean = false;
+  private vec: Vector2Like = { x: 0, y: 500 };
+  private g: number = 0;
 
+  public getDead(): boolean {
+    return this.isDead;
+  }
+
+  public setDead(dead): void {
+    this.isDead = dead;
+  }
+
+  initAnimation() : void {
+    this.anims.animationManager.create({
+        key: "left",
+        frames: this.anims.animationManager.generateFrameNumbers("santa", {
+          start: 0,
+          end: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+  
       this.anims.animationManager.create({
-        key: 'left',
-        frames: this.anims.animationManager.generateFrameNumbers('santa', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    
-    this.anims.animationManager.create({
-        key: 'turn',
-        frames: [ { key: 'santa', frame: 4 } ],
+        key: "turn",
+        frames: [{ key: "santa", frame: 4 }],
         frameRate: 20
-    });
-    
-    this.anims.animationManager.create({
-        key: 'right',
-        frames: this.anims.animationManager.generateFrameNumbers('santa', { start: 5, end: 8 }),
+      });
+  
+      this.anims.animationManager.create({
+        key: "right",
+        frames: this.anims.animationManager.generateFrameNumbers("santa", {
+          start: 5,
+          end: 8
+        }),
         frameRate: 10,
         repeat: -1
-    });
+      });
+  }
 
-      // image
-      //this.setScale(3);
-      this.setOrigin(0, 0);
-      // physics
-      params.scene.physics.world.enable(this);
-      (<Phaser.Physics.Arcade.Body> this.body).setBounce(0,0);
-      (<Phaser.Physics.Arcade.Body> this.body).setCollideWorldBounds(true);
-      (<Phaser.Physics.Arcade.Body> this.body).setAllowGravity(true);
-      
-      // animations & tweens
-      this.anim = [];
+  constructor(params) {
+    super(params.scene, params.x, params.y, params.key, params.frame);
+
+    this.initAnimation()
+
+    // physics
+    params.scene.physics.world.enable(this);
+
+    this.setOrigin(0, 0);
+    (<Phaser.Physics.Arcade.Body>this.body).setBounce(0, 0);
+    (<Phaser.Physics.Arcade.Body>this.body).setCollideWorldBounds(true);
+    (<Phaser.Physics.Arcade.Body>this.body).setAllowGravity(true);
+
+    // animations & tweens
+    this.anim = [];
     //   this.anim.push(
     //     params.scene.tweens.add({
     //       targets: this,
@@ -59,54 +68,58 @@ export class Santa extends Phaser.GameObjects.Sprite {
     //       angle: 1
     //     })
     //   );
-  
 
-      // input
-      this.jumpKey = params.scene.input.keyboard.addKey(
-        Phaser.Input.Keyboard.KeyCodes.SPACE
+    // input
+    this.jumpKey = params.scene.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+
+    this.cursors = params.scene.input.keyboard.createCursorKeys();
+    //this.pointers = params.scene.input.activePointer;
+
+    this.addGyro();
+    params.scene.input.on("pointerdown", pointer => {
+      this.isPointerDown = true;
+    });
+    params.scene.input.on("pointerup", pointer => {
+      this.isPointerDown = false;
+    });
+    params.scene.add.existing(this);
+  }
+
+  public addGyro(): void {
+    if (gyro) {
+      gyro.frequency = 10;
+      // start gyroscope detection
+      gyro.startTracking(
+        (function(parent) {
+          // updating player velocity
+          return function(o) {
+            if (o && o.gamma) {
+              parent.g = o.gamma;
+            }
+          };
+          //player.body.velocity.y += o.beta/20;
+        })(this)
       );
-  
-      this.cursors = params.scene.input.keyboard.createCursorKeys();
-      //this.pointers = params.scene.input.activePointer;
-      
-      this.addGyro();
-      params.scene.input.on('pointerdown', (pointer) => { this.isPointerDown = true; });
-      params.scene.input.on('pointerup', (pointer) => { this.isPointerDown = false; });
-     params.scene.add.existing(this);
     }
-
-    public addGyro():void{
-        if(gyro){
-            gyro.frequency = 10;
-            // start gyroscope detection
-            gyro.startTracking(function(parent) {
-                // updating player velocity
-                return function(o){
-                    if(o && o.gamma){
-                        parent.g = o.gamma;
-                    }
-                };
-                //player.body.velocity.y += o.beta/20;
-            }(this));	
-        }
-        
-    }
+  }
 
   public handleInput(): void {
-    if(this.cursors.up.isDown || this.isPointerDown) {
-        (<Phaser.Physics.Arcade.Body> this.body).setVelocityY(-this.vec.y);
-        this.vec.y+=10;
-    }else if (this.cursors.right.isDown || this.g > 0) {
-        this.vec.y = 100;
-        (<Phaser.Physics.Arcade.Body> this.body).setVelocityX(300);
-        this.anims.play('right',true);
-    }else if (this.cursors.left.isDown || this.g < 0) {
-        this.vec.y = 100;
-        (<Phaser.Physics.Arcade.Body> this.body).setVelocityX(-300);
-        this.anims.play('left',true);
-    }else if(this.cursors.down.isDown) {
-        (<Phaser.Physics.Arcade.Body> this.body).setVelocityY(this.vec.y);
-        this.vec.y = 100;
+    if (this.cursors.up.isDown || this.isPointerDown) {
+      (<Phaser.Physics.Arcade.Body>this.body).setVelocityY(-this.vec.y);
+      this.vec.y += 10;
+    } else if (this.cursors.right.isDown || this.g > 0) {
+      this.vec.y = 100;
+      (<Phaser.Physics.Arcade.Body>this.body).setVelocityX(300);
+      this.anims.play("right", true);
+    } else if (this.cursors.left.isDown || this.g < 0) {
+      this.vec.y = 100;
+      (<Phaser.Physics.Arcade.Body>this.body).setVelocityX(-300);
+      this.anims.play("left", true);
+    } else if (this.cursors.down.isDown) {
+      (<Phaser.Physics.Arcade.Body>this.body).setVelocityY(this.vec.y);
+      this.vec.y = 100;
     }
 
     // if (this.jumpKey.isDown) {
@@ -114,14 +127,14 @@ export class Santa extends Phaser.GameObjects.Sprite {
     //   }
   }
 
-    update(): void {
-      //this.handleInput();
-      //this.isOffTheScreen();
-    }
-  
-    private isOffTheScreen(): void {
-      if (this.y + this.height > this.scene.sys.canvas.height) {
-        this.isDead = true;
-      }
+  update(): void {
+    //this.handleInput();
+    //this.isOffTheScreen();
+  }
+
+  private isOffTheScreen(): void {
+    if (this.y + this.height > this.scene.sys.canvas.height) {
+      this.isDead = true;
     }
   }
+}
--- src/santa/scenes/mainScene.ts
+++ src/santa/scenes/mainScene.ts
@@ -1,67 +1,44 @@
 import { Santa } from "../objects/santa";
-
-/**
- * @author       Digitsensitive <digit.sensitivee@gmail.com>
- * @copyright    2018 Digitsensitive
- * @license      Digitsensitive
- */
+import { Panel } from "../objects/panel";
 
 export class MainScene extends Phaser.Scene {
   private phaserSprite: Phaser.GameObjects.Sprite;
 
-  // objects
+  // Chara
   private santa: Santa;
-  // private pipes: Phaser.GameObjects.Group;
-  // private bg: Phaser.GameObjects.TileSprite;
 
+  // Phaser.Physics.Arcade.StaticGroup;
+  private panels: Phaser.GameObjects.Group;
+
+  // Back Ground
+  private bg: Phaser.GameObjects.TileSprite;
+  
   // variables
   private timer: Phaser.Time.TimerEvent;
   private score: number;
   private scoreText: Phaser.GameObjects.Text[];
   
-
   constructor() {
     super({
       key: "MainScene"
     });
   }
 
-  preload(): void {
-    //this.load.image("logo", "./assets/santa-sd-720.png");
-    //this.load.image("logo", "./assets/dummy.png");
-    this.load.image("bg", "./assets/SantaBG-png256.png");
-    this.load.spritesheet('santa', './assets/santa-sd-720.png', {
-      frameWidth: 80,
-      frameHeight: 97
-    });
-    this.load.spritesheet('snowflakes', './assets/external/snowflakes.png', {
-      frameWidth: 17,
-      frameHeight: 17
-    });
-    this.load.spritesheet('snowflakes_large', './assets/external/snowflakes_large.png', {
-      frameWidth: 64,
-      frameHeight: 64
-    });
-    this.load.audio("bgm", './assets/bgm.mp3');
+  init(): void {
+    // objects
+    this.santa = null;
+    this.panels = this.add.group({ classType: Panel });
+    this.bg = null;
 
+    // variables
+    this.timer = undefined;
+    this.score = -1;
+    this.scoreText = [];
   }
 
-  max = 0;
-  front_emitter;
-  mid_emitter;
-  back_emitter;
-  update_interval = 4 * 60;
-  i = 0;
 
-  create(): void {
-    //this.phaserSprite = this.add.sprite(<number> this.game.config.width,<number> this.game.config.height, "logo");
-    
-    this.add.image(0, 0, 'bg');
-    //this.add.sprite(200,200,'santa');
-    this.sound.add('bgm');
-    this.sound.play('bgm');
-    var particles = this.add.particles('snowflakes_large');
-
+  createSnow():void{
+    let particles = this.add.particles('snowflakes_large');
     particles.createEmitter({
         x: { min: 0, max: 400 },
         y: { min: 0, max: 20 },
@@ -74,6 +51,11 @@ export class MainScene extends Phaser.Scene {
         blendMode: Phaser.BlendModes.NORMAL,
         rotate: { min: 0, max: 100 },
     });
+  }
+
+  create(): void {
+    this.bg = this.add.tileSprite(0, 0, this.cameras.main.width, this.cameras.main.height, "bg");
+    //let panels = this.physics.add.staticGroup();
 
     this.santa = new Santa({
       scene: this,
@@ -82,13 +64,46 @@ export class MainScene extends Phaser.Scene {
       key: 'santa'
     });
 
-  }
+    this.generatePanels();
 
+    this.sound.add('bgm');
+    this.sound.play('bgm');
+
+    this.createSnow();
+
+    this.physics.add.collider(this.santa, this.panels);
+  }
 
   update(): void{
     this.santa.handleInput();
   }
 
+  generatePanels(): void{
+    // update the score
+    this.score += 1;
+    this.scoreText[0].setText("" + this.score);
+    this.scoreText[1].setText("" + this.score);
+
+    // randomly pick a number between 1 and 5
+    let x = Math.floor(Math.random() * this.cameras.main.width);
+    let y = Math.floor(Math.random() * this.cameras.main.height);
+
+    this.generateOnePanel(x,y, 0)
+  }
+
+  private generateOnePanel(x, y, frame): void {
+    // create a pipe at the position x and y
+    let panel = new Panel({
+      scene: this,
+      x: x,
+      y: y,
+      frame: frame,
+      key: "panel"
+    });
+
+    // add pipe to group
+    this.panels.add(panel);
+  }
 
 
 }
